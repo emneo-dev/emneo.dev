11e1c1e14328376a7132dc927acdf29a7201baf2
Huntears <alexandre.flion@epitech.eu>
2020-02-03 12:03:39 +0100

    [FIX]: weird js

--- js/script.js
+++ js/script.js
@@ -1,13 +1,7 @@
-document.addEventListener("DOMContentLoaded", event => {
-	navigator.clipboard.writeText("h").then(
-		() => {
-			console.log('[SPECTRAPULSE] Added "h" to clipboard!');
-});})
-
 const discord = (discord_username) => {
 	navigator.clipboard.writeText(discord_username).then(
-		() => { 
+		() => {
 			alert(`I added my username to your clipboard! (${discord_username})`);
 		}, () => {
 			alert(`My Discord Username is ${discord_username}!`);
-})}
+});}
