00ce9e40a12a047e874566314f7dbc9d0fa25bd1
Huntears <dev@huntears.com>
2021-09-04 23:32:09 +0200

    Simplify codebase

--- js/script.js
+++
@@ -1,7 +0,0 @@
-const discord = (discord_username) => {
-	navigator.clipboard.writeText(discord_username).then(
-		() => {
-			alert(`I added my username to your clipboard! (${discord_username})`);
-		}, () => {
-			alert(`My Discord Username is ${discord_username}!`);
-});}
